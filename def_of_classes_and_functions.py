9e8a41ce48c35bcbf66037a558f56ec3aff5ffb0
Xameleonnn <xameleonnn@gmail.com>
2021-07-27 16:31:49 +0300

    2nd day of work

--- def_of_classes_and_functions.py
+++ def_of_classes_and_functions.py
@@ -7,20 +7,28 @@ Created on Mon Jul 26 16:28:42 2021
 
 class Item:
     
-    def __init__(self, value, weight):
+    def __init__(self, item_name, value, weight, level):
+        self.item_name = item_name
         self.value = value
         self.weight = weight
+        self.level = level
         
     def get_value(self):
-        return self.get_value
+        return self.value
     
     def get_weight(self):
         return self.weight
     
+    def get_item_name(self):
+        return self.item_name
+    
+    def get_level(self):
+        return self.level
+    
 class CollectionItem(Item):
     
-    def __init__(self, value, weight, collection):
-        Item.__init__(self, value, weight)
+    def __init__(self, item_name, value, weight, level, collection):
+        Item.__init__(self, item_name, value, weight, level)
         self.collection = collection
         
     def get_collection(self):
@@ -30,74 +38,140 @@ class Location:
     
     def __init__(self, level):
         self.level = level
+        self.items = get_list_of_items_lvl(level)
         
     def get_level(self):
         return self.level
     
-bag = []
-weight_counter = 0
-value_counter = 0
-max_weight = 100
-list_of_items = []
+    def get_items(self):
+        return self.items
+
+def get_list_of_items_lvl(level):
+    '''
+    For putting items with right item_level
+    into location with it's initialization
+    
+    Parameters: level - integer
+    
+    Returns: list - list of objects which type is Item
+    '''
+    list_of_items_req_level = []
+    for element in list_all_items:
+        if element.get_level() == level:
+            list_of_items_req_level.append(element)
+            
+    return list_of_items_req_level
+
+# Items
 
 # Items for lvl 1 locations
-glass_bottle = Item(2,1)
-brick = CollectionItemItem(5, 3, 'bars')
-blank_scroll = CollectionItem(1, 0, 'scholar junk')
+glass_bottle = Item('A botte', 2, 1, 1)
+brick = CollectionItem('A brick', 5, 3, 1, 'bars')
+blank_scroll = CollectionItem('A blank scroll', 1, 0, 1, 'scholar junk')
 
 # Items for lvl 2 locations
-inkwell = CollectionItem(6, 1, 'scholar junk')
-metal_goblet = CollectionItem(8, 2, 'tableware')
-copper_bar = CollectionItem( , , 'bars')
+inkwell = CollectionItem('An inkwell', 6, 1, 2, 'scholar junk')
+metal_goblet = CollectionItem('A metal goblet', 8, 2, 2, 'tableware')
+copper_bar = CollectionItem('Copper bar', 12, 5, 2, 'bars')
 
 # Items for lvl 3 locations
-silver_fork = CollectionItem(8, 2, 'tableware')
-strange_shroom = CollectionItem(, , 'strange')
-bat_man_wing = CollectionItem(, , 'trophy')
+silver_fork = CollectionItem('A silver fork', 8, 1, 3, 'tableware')
+strange_shroom = CollectionItem('A strange glowing mushroom', 0, 0, 3, 'strange')
+bat_man_wing = CollectionItem('Wing of a bat man', 20, 10, 3, 'trophy')
 
 # Items for lvl 4 locations
-pearl = Item( , )
-encrusted_silver_bracelet = Item( , )
-siedge_crossbow = CollectionItem(, , 'weaponry')
+pearl = Item('A perl', 10, 1, 4)
+encrusted_silver_bracelet = Item('A silver bracelet with encrusted gems', 15, 2, 4)
+siedge_crossbow = CollectionItem('A siedge crossbow', 30, 10, 4, 'weaponry')
 
 # Items for lvl 5 locations
-emerald = Item( , )
-astrology_book = CollectionItem(, , 'scholar junk')
-giant_strange_helmet = CollectionItem(, , 'strange')
+emerald = Item('Cut emerald', 15, 1, 5)
+astrology_book = CollectionItem('A book about astrology', 20, 2, 5, 'scholar junk')
+giant_strange_shroom = CollectionItem('A strange giant glowing mushroom', 0, 20, 5, 'strange')
 
 # Items for lvl 6 locations
-telescope = CollectionItem(, , 'scholar junk')
-unicorn_horn = CollectionItem(, , 'trophy')
-small_steel_anvil = Item( , )
+telescope = CollectionItem('A telescope', 40, 3, 6, 'scholar junk')
+unicorn_horn = CollectionItem('A horn of a unicorn', 50, 3, 6, 'trophy')
+crate_with_steel = Item('A wooden crate with a lot of steel bars in it', 250, 50, 6)
 
 # Items for lvl 7 locations
-golden_spoon = CollectionItem(, , 'tableware')
-strange_staff = CollectionItem(, , 'strange')
-steel_zweihander = CollectionItem(, , 'weaponry')
+golden_spoon = CollectionItem('A golden spoon', 35, 1, 7, 'tableware')
+strange_staff = CollectionItem('A strange glowing staff', 0, 5, 7, 'strange')
+steel_longsword = CollectionItem('A stell longsword', 120, 10, 7, 'weaponry')
 
 # Items for lvl 8 locations
-black_pearl = Item( , )
-gold_bar = CollectionItemItem(, , 'bars')
-giant_silver_plate = CollectionItem(, , 'tableware')
+black_pearl = Item('A black pearl', 100, 1, 8)
+gold_bar = CollectionItem('A gold bar', 250, 5, 8, 'bars')
+giant_silver_plate = CollectionItem('A giant silver dish with encrusted gems', 400, 10, 8, 'tableware')
 
 # Items for lvl 9 locations
-platinum_ring = Item( , )
-strange_glowing_stone = CollectionItem(, , 'strange')
-irridium_bar = 
+platinum_ring = Item('A platinum ring with ruby', 150, 1, 9)
+strange_glowing_stone = CollectionItem('A strange glowing stone', 0, 15, 9, 'strange')
+massive_irridium_cube = CollectionItem('A massive cube made of metal irridium', 650, 50, 9, 'bars')
 
 # Items for lvl 10 locations
-super_alloy_dagger = CollectionItem(, , 'weaponry')
-minotaur_head = CollectionItem(, , 'trophy')
-golden_chest = Item( , )
+titan_alloy_dagger = CollectionItem('A dagger made of unknown alloy', 500, 3, 10, 'weaponry')
+minotaur_head = CollectionItem('A head of minotaur', 1000, 25, 10, 'trophy')
+treasure_chest = Item('A golden chest filled with coins', 2500, 80, 10)
 
-def put_in_bag(item):
-    item_value = item.get_value()
-    item_weight = item.get_weight()
-    suggested_weight = weight_counter + item_weight
-    if suggested_weight < max_weight:
-        value_counter += value
-        weight_counter += weight
-        bag.append(item)
-        print('You put item in your precious bag successfully.')
-    else:
-        print("You can't put item in your damn bag, it's too much weight!")
+list_all_items = [glass_bottle, brick,blank_scroll, inkwell, metal_goblet, copper_bar, silver_fork, bat_man_wing, pearl,
+                  encrusted_silver_bracelet, siedge_crossbow, emerald, astrology_book, giant_strange_shroom, telescope, 
+                  unicorn_horn, crate_with_steel, golden_spoon, strange_staff, steel_longsword, black_pearl, gold_bar, 
+                  giant_silver_plate, platinum_ring, strange_glowing_stone, massive_irridium_cube, titan_alloy_dagger, 
+                  minotaur_head, treasure_chest]
+
+# Locations
+    
+# Locations lvl 1
+cave_l1 = Location(1)
+basement_l1 = Location(1)
+
+# Locations lvl 2
+flooded_cave_l2 = Location(2)
+cave_l2 = Location(2)
+basement_l2 = Location(2)
+
+# Location lvl 3
+flooded_cave_l3 = Location(3)
+antient_tomb_l3 = Location(3)
+maze_l3 = Location(3)
+
+# Location lvl 4
+ravine_l4 = Location(4)
+undeground_swamp_l4 = Location(4)
+antient_tomb_l4 = Location(4)
+
+# Location lvl 5
+undeground_suburbs_l5 = Location(5)
+
+# Location lvl 6
+barracks_l6 = Location(6)
+inn_l6 = Location(6)
+temple_l6 = Location(6)
+
+# Location lvl 7
+prison_l7 = Location(7)
+square_l7 = Location(7)
+graveyard_l7 = Location(7)
+
+# Location lvl 8
+catacombs_l8 = Location(8)
+minotaur_labyrinth_l8 = Location(8)
+spyder_caves_l8 = Location(8)
+
+# Location lvl 9
+escape_tunned_l9 = Location(9)
+escape_elevator_l9 = Location(9)
+
+# Location lvl 10
+dragon_test_l10 = Location(10)
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -5,8 +5,130 @@ Created on Mon Jul 26 10:57:15 2021
 @author: Михаил
 """
 import random
-# from def_of_classes_and_functions import * 
+from def_of_classes_and_functions import * 
+'''
+Initializing some values for game to work properly and for balance 
+purposes. If you want to shift game balance - change next objects:
+    max_weight, Items_for_lvl_x_locations
+'''
+bag = []
+weight_counter = 0
+value_counter = 0
+max_weight = 100
+list_of_items = []
 
+def get_game_rules():
+    '''
+    Just printing our the rules
+    
+    Returns: None
+    '''
+    print('Your main goal is to get as much value from the items as possible. '
+          'But remember, you cannot take more then', max_weight, 'of weight. '
+          'Rules are as follows: '
+          '1 - You can pick only 1 item from suggested. '
+          '2 - Once obtained, you cannot throw the item away from the bag '
+          'because of extreme greed that you possess. '
+          '3 - Some items can be part of a collection. Gather items from '
+          'one collection to get more value points at final scoring. '
+          )
+
+def put_in_bag(item):
+    '''
+    Function for putting the item(object) in the bag(list)
+    while checking for possible overweight.
+    
+    Parameters: item - object of a class Item
+    
+    Returns: None
+    '''
+    item_weight = item.get_weight()
+    suggested_weight = weight_counter + item_weight
+    if suggested_weight < max_weight:
+        bag.append(item)
+        print('You put the item in your precious bag successfully.')
+    else:
+        print("You can't put item in your damn bag, it's too much weight!")
+
+def display_item_props(loot):
+    '''
+    Function for getting all relevant properties
+    of the item(object) and printing them to a player
+    
+    Parameters: loot - list with objects of type Item
+    
+    Returns: None
+    '''
+    for element in loot:
+        item_name = element.get_item_name()
+        item_value = element.get_value()
+        item_weight = element.get_weight()
+        print("Item's name is", item_name + ', ', 
+              "it's value is", item_value, ', ', 
+              "it's weight is", item_weight, '.')
+    
+def get_current_weight(bag):
+    '''
+    Function for getting current weight load
+    
+    Parameters: bag - list with objects of type Item
+    
+    Returns: int
+    '''
+    resulting_weight = 0
+    for item in bag:
+        item_weight = item.get_weight()
+        resulting_weight += item_weight
+    
+    return resulting_weight
+
+def process_next_move():
+    '''
+    Function asks for the next move. A player can choose his path through input, check 
+    his inventory (which is bag[items]), check for his load and learn about gamerules.
+    
+    Returns: str - 'r' or 'l'
+    '''
+    complition = False
+    while complition != True:
+        print('===================================================')
+        print('So,', name + ', what would you like to do? '
+              '1 - go right, '
+              '2 - go left, '
+              '3 - check bag for items, '
+              '4 - check for current weight load, '
+              '5 - check game rules.'
+              )
+        decision = int(input('Make your choise! '))
+        print('===================================================')
+        print('You picked', decision)
+        possible_choices = [1, 2, 3, 4, 5]
+        while decision not in possible_choices:
+            decision = int(input("You were not the smartest kid in the family, right? "
+                                 "Type the goddamn NUMBER! (from 1 to 5): ")) # Don't be shy to show some disrespect for a greedy goblin
+            print('You picked', decision)
+       
+        if decision == 1:
+            return 'r'
+        elif decision == 2:
+            return 'l'
+        elif decision == 3:
+            if len(bag) == 0:
+                print('There is nothing in the bag.')
+            else:
+                print('Here are some precious items in your bag:')
+                display_item_props(bag)
+        elif decision == 4:
+            print('Current weight is', get_current_weight(bag), '. ',
+                  'You can take', max_weight - get_current_weight(bag),
+                  'more points of weight')
+            
+        elif decision == 5:
+            get_game_rules()
+            
+        else:
+            print('Unexpected outcome, lets try again.')
+    
 print('I greet you, little greedy creature. '
       'You have come with nothing but an empty '
       'bag and desire to become rich. Let us begin!')
@@ -21,8 +143,44 @@ random_int = random.randint(0, 5)
 randomised_nickname = nickname.pop(random_int)
 name = input_name + ' ' + randomised_nickname
 print('From now on, you are', name + '.') # + is for dot to appear without space
-decision = input('Go ahead and pick your way, right or left. (r/l) ')
-print(decision)
+
+# Here we start a jorney
+
+print('You see an entrance in a cave on your left and a small dilapidated cabin on your right')
+player_pick = process_next_move()
+# level = 0
+
+if player_pick == 'r': # basement_l1
+    print('You enter a cabin. It is dark and empty here but you notice '
+          "a ladder, which leads into cabin's basement. You are going "
+          "down the ladder and found yourself in a mossy dump basement. "
+          "There are some items lying around. "
+          'Those are not yours, but you cannot resist your kleptomania.')
+    
+    loot = basement_l1.get_items()
+    display_item_props(loot)
+    player_item_pick = int(input('Which item do you choose? (1, 2, 3): '))
+    put_in_bag(loot.pop(player_item_pick-1))
+    
+    print('After picking the item up, you notice a hole in a wall to the right '
+          'and a ladder deeper down to the left.')
+    player_pick = process_next_move()
+    if player_pick == 'r': # cave_l2
+        # something for cave_l2
+        pass
+else: # cave_l1
+    print("You enter a massive cave. An underground river block your way. "
+          "There are some items lying just past the entrance.")
+    
+    loot = cave_l1.get_items()
+    display_item_props(loot)
+    player_item_pick = int(input('Which item do you choose? (1, 2, 3): '))
+    put_in_bag(loot.pop(player_item_pick-1))
+    print("You can cross the river to ford on the left, where it's not "
+          "so deep or you can try to cross a wooden rotten bridge on the right")
+     
+    if player_pick == 'r': # cave_l2
+        # again something for cave_l2, i need to think how not to repeat myself
 
 
 
